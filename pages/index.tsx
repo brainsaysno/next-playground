7965a9a3b5332f0d647fefc761b0affb8cd39435
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2022-07-17 13:27:10 -0300

    fix: center next/link in main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
-
 import type { NextPage } from "next";
 import Link from "next/link";
 
 const Home: NextPage = () => {
-
   return (
     <div>
       <main className="flex flex-col justify-center align-center w-screen h-screen px-32">
         <h1 className="text-3xl font-semibold text-center py-5">
           UI Playground
         </h1>
-        <Link href="/projects" className="text-center">Go to projects &rarr;</Link>
+        <Link href="/projects">
+          <a className="text-center">Go to projects &rarr;</a>
+        </Link>
       </main>
     </div>
   );
